ab2eec42b17d5091af6ea1bbe6eb748fa684f6bc
Brad Leupen <brad@entrinsik.com>
2018-10-11 17:13:19 -0400

    added acme user db and domain

--- index.js
+++ index.js
@@ -2,48 +2,54 @@
 
 const path = require('path');
 
-exports.register = function(server, opts, next) {
-  let bundle = server.bundle('acme').scan(__dirname, 'public');
+exports.register = function (server, opts, next) {
+    let bundle = server.bundle('acme').scan(__dirname, 'public');
 
-  server.injector().inject(bundle);
+    server.injector().inject(bundle);
 
-  server.select('content').route({
-    method: 'GET',
-    path: '/assets/acme/images/{path*}',
-    config: {
-      auth: false,
-      handler: {
-        directory: {
-          path: path.resolve(__dirname, './public/images'),
-          listing: false,
-          index: false
+    server.select('content').route({
+        method: 'GET',
+        path: '/assets/acme/images/{path*}',
+        config: {
+            auth: false,
+            handler: {
+                directory: {
+                    path: path.resolve(__dirname, './public/images'),
+                    listing: false,
+                    index: false
+                }
+            }
         }
-      }
-    }
-  });
+    });
 
-  server.route({
-    path: '/login.html',
-    method: 'get',
-    config: {
-      auth: false,
-      handler: {
-        file: path.resolve(__dirname, 'assets/login.html')
-      }
-    }
-  });
+    server.route({
+        path: '/login.html',
+        method: 'get',
+        config: {
+            auth: false,
+            handler: {
+                file: path.resolve(__dirname, 'assets/login.html')
+            }
+        }
+    });
 
-  server.route({
-    path: '/images/informer.svg',
-    method: 'get',
-    config: {
-      auth: false,
-      handler: {
-        file: path.resolve(__dirname, 'assets/acme.svg')
-      }
-    }
-  });
-  next();
+    server.route({
+        path: '/images/informer.svg',
+        method: 'get',
+        config: {
+            auth: false,
+            handler: {
+                file: path.resolve(__dirname, 'assets/acme.svg')
+            }
+        }
+    });
+
+    server.on('start', () => {
+        server.driver('domain', require('./lib/user-auth-domain')(server));
+        server.driver('systemFeature', require('./lib/user-auth-feature')(server))
+    });
+
+    next();
 };
 
 exports.register.attributes = { name: 'informer-acme' };
--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  function acmeConfig($mdThemingProvider) {
+  function acmeConfig($mdThemingProvider, componentProvider) {
     // Angular material design theme
     $mdThemingProvider
       .theme('default')
@@ -10,6 +10,8 @@
       .accentPalette('green', { default: '500' });
 
     $('body').addClass('acme-informer');
+
+      componentProvider.component('acmeLogin', '<md-input-container class="md-block" md-no-float><md-icon md-font-icon="material-icons">person</md-icon><input name="username" ng-model="$component.ngModel.auth.username" type="text" placeholder="Username"> </md-input-container><md-input-container class="md-block" md-no-float><md-icon md-font-icon="material-icons">lock</md-icon><input name="password" ng-model="$component.ngModel.auth.password" type="password" placeholder="Password"></md-input-container>');
   }
 
   angular.module('informer').config(acmeConfig);
